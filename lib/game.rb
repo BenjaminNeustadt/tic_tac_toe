153dd521cafa6758664ebdc1606337c54d9867c8
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-01-12 15:27:28 +0000

    WIP designing logic for place method

--- lib/game.rb
+++ lib/game.rb
@@ -2,17 +2,23 @@ require_relative './board'
 
 class Game
 
-
-def initialize
-  @board = Board.new.display
-end
+  def initialize
+    @board = Board.new.display
+  end
 
 attr_reader :board
 
 
-def place()
+  def place(position)
 
-end
+    if position == 1 
+      @board = ["x", nil, nil, nil, nil, nil, nil, nil, nil]
+    elsif position == 2
+      @board = [nil, "x", nil, nil, nil, nil, nil, nil, nil]
+    elsif position == 9
+      @board = [nil, nil, nil, nil, nil, nil, nil, nil, "x"]
+    end
 
+  end
 
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -9,4 +9,31 @@ RSpec.describe Game do
     end
   end
 
+  context 'place method' do
+    it 'is able to add a piece to position 1' do
+      game = Game.new
+      game.place(1)
+      expected = ["x", nil, nil, nil, nil, nil, nil, nil, nil]
+      actual = game.board
+      expect(actual).to eq expected
+    end
+
+    it 'is able to add a piece to position 2' do
+      game = Game.new
+      game.place(2)
+      expected = [nil, "x", nil, nil, nil, nil, nil, nil, nil]
+      actual = game.board
+      expect(actual).to eq expected
+    end
+
+    it 'is able to add a piece to position 9' do
+      game = Game.new
+      game.place(9)
+      expected = [nil, nil, nil, nil, nil, nil, nil, nil, "x"]
+      actual = game.board
+      expect(actual).to eq expected
+    end
+  end
+
+
 end
